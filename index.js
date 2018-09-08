be8cc2c6346d6494f93bd9c9ce90484719c7dc5a
Mike Curtin <curtinmjc@gmail.com>
2018-09-08 17:24:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Add your functions and code here
-function destructivelyAppendKitten(array, name) {
-  array.push(name);
-  return array;
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+  return kittens;
 }
